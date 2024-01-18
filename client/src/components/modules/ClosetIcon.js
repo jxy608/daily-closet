a75ee77f9297d90150627a68cb9fea5b20cc5608
emilychen <emilymchen@gmail.com>
2024-01-18 16:49:59 -0500

    add temporary closet icon to redirect to closet space

--- client/src/components/modules/ClosetIcon.js
+++ client/src/components/modules/ClosetIcon.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link, Navigate } from "react-router-dom";
 
+import "../../utilities.css";
+import "./ClosetIcon.css";
+import closetIcon from "../../../assets/closet-temporary.png";
+
 const ClosetIcon = () => {
   return (
     <div>
-      <h2>closet</h2>
-      <p>
-        <Link to={`/closet`}>go to the closet</Link>
-      </p>
+      <Link to="/closet" className="closet-link">
+        <h2>closet</h2>
+        <img src={closetIcon} width="70%" height="70%" />
+      </Link>
     </div>
   );
 };
