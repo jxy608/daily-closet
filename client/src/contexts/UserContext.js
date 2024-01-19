7ceaf2a17bbe6fd3fd26c0d010d4ef03cd5de874
emilychen <emilymchen@gmail.com>
2024-01-19 15:05:38 -0500

    save usercontext, and access in settings. updating usercontext + database is still broken

---
+++ client/src/contexts/UserContext.js
@@ -0,0 +1,28 @@
+// UserContext.js
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { get } from "../utilities.js"; // Adjust the import path as necessary
+
+import { useAuth } from "./AuthContext";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const { userId } = useAuth();
+
+  useEffect(() => {
+    get("/api/user", { userId: userId })
+      .then((userData) => {
+        setUser(userData);
+        console.log("setting user context");
+        console.log(userData);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch user data: ", error);
+      });
+  }, [userId]);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export const useUser = () => useContext(UserContext);
