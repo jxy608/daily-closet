a3dec8b730493a7d3a10caf985857c95b78d5d0b
emilychen <emilymchen@gmail.com>
2024-01-30 17:11:07 -0500

    add closet open on hover

--- client/src/components/modules/ClosetIcon.js
+++ client/src/components/modules/ClosetIcon.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 
 import "../../utilities.css";
 import "./ClosetIcon.css";
-import closetIcon from "../../../assets/Untitled_Artwork.png";
+import closetClosed from "../../../assets/closet-closed.png";
+import closetOpen from "../../../assets/closet-open.png";
 
 const ClosetIcon = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div>
       <Link to="/closet" className="closet-link">
         <h2>closet</h2>
-        <img src={closetIcon} width="100%" height="100%" />
-        {/* <img src={closetIcon} width="70%" height="70%" /> */}
+        <img
+          className="closet-icon"
+          src={isHovered ? closetOpen : closetClosed}
+          width="100%"
+          height="100%"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        />
+        {/* <div className="closet-icon"></div> */}
       </Link>
     </div>
   );
