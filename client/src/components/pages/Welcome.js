96276da0b06acb9193d8b68e078e322a53595e9c
emilychen <emilymchen@gmail.com>
2024-01-31 11:20:27 -0500

    update welcome page

--- client/src/components/pages/Welcome.js
+++ client/src/components/pages/Welcome.js
@@ -10,6 +10,7 @@ import ClosetSection from "../../../assets/closet_section.png";
 import UploadClothing from "../../../assets/upload_clothing.png";
 import Homepage from "../../../assets/homepage.png";
 import Closet from "../../../assets/closet.png";
+import LaundryDemo from "../../../assets/laundry-demo.png";
 
 const Welcome = ({ userId, handleLogout }) => {
   const navigate = useNavigate();
@@ -22,15 +23,17 @@ const Welcome = ({ userId, handleLogout }) => {
     Closet,
     ClosetSection,
     Homepage,
+    LaundryDemo,
     ClosetIcon,
   ];
   const content = [
     "You wake up in the morning, still groggy from staying up late to finish up your psets. Your blinds are closed, and your brain is dead. As you open your closet, the Jane Street t-shirt in the front seems to speak to you, but you vaguely recall already having worn it twice this week. You sniff the armpits. Is three times a week too much? What if it’s cold outside? What in the world do you wear?",
     "Daily Closet is your one-stop-shop for closet management and daily outfit recommendations, so you can know what to wear without having to use your brain. Whether it’s sun, rain, or snow, you’ll have a perfect outfit ready to go!",
-    "As a new user to Daily Closet, after inputting your user settings, you will upload photos of your clothes, which you can then categorize and adjust based on the appropriate temperature range to wear the outfit in.",
+    "As a new user to Daily Closet, after inputting your user settings, you will upload photos of your clothes, which you can then categorize and adjust based on the appropriate temperature range to wear the clothing item in.",
     "You can view and manage your closet and all of the clothes you’ve uploaded to the website on our closet page, which is categorized into sections to be easily accessible. For example, if you want to view all of your tops, click on the “tops” section of the closet to access all of the clothes you have uploaded. You’ll also be able to add new articles of clothing in this page after a good retail therapy session.",
-    "Each of the clothing categories will bring up a simple pop-up menu where you can scroll through your catalog of clothes. You can then edit your individual clothes through this menu if you realize that the sweater your mom gave you wasn’t quite as warm as you thought it would be.",
-    "Last but certainly not least is the homepage you'll access everything from. Here, you’ll get a daily outfit recommended to you based on the temperature and weather in your location. If you don’t like the outfit generated, you can shuffle to get a new one recommended to you. Daily Closet keeps track of the number of times you’ve accepted or declined to wear certain articles of clothing, and will recommend you sell or donate clothes you don’t wear often to free up closet space! (See the recycle button on the closet page for more details.)",
+    "Each of the clothing categories will bring up a simple pop-up menu where you can scroll through your catalog of clothes. You can then edit your individual clothes through this menu if you realize that the sweater your mom gave you wasn’t quite as warm as you thought it would be!",
+    "On the homepage, you’ll get a daily outfit recommended to you based on the temperature and weather in your location. If you don’t like the outfit generated, you can click the X to get a new one recommended to you, or hit the check to accept. Daily Closet keeps track of the number of times you’ve declined to wear certain articles of clothing, and will recommend you sell or donate clothes you don’t wear often to free up closet space! (See the recycle button on the closet page for more details.)",
+    "Daily Closet also keeps track of how many times you've worn your clothes, and lets you know at a glance when laundry day is coming up! You can also view which clothes are in the laundry, and can wash them to reset the number of wears through the laundry menu.",
     "Daily Closet is for any person at any age. Whether you’re an MIT student whose closet largely consists of free career fair t-shirts or a mid-career professional looking to up your fashion game while using less brainpower, Daily Closet is here to guide you at any step along the way. Click begin to start your journey now!",
   ];
 
@@ -44,7 +47,6 @@ const Welcome = ({ userId, handleLogout }) => {
 
   const handleBegin = () => {
     navigate(`/settings/welcome`);
-
   };
 
   return (
