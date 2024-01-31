dbb7ad497975a4b2d73f196dbfba564f6677a4a3
emilychen <emilymchen@gmail.com>
2024-01-31 06:06:55 -0500

    screaming crying

--- client/src/components/modules/ClosetIcon.js
+++ client/src/components/modules/ClosetIcon.js
@@ -4,7 +4,7 @@ import { Link, Navigate } from "react-router-dom";
 import "../../utilities.css";
 import "./ClosetIcon.css";
 import closetClosed from "../../../assets/closet-closed.png";
-import closetOpen from "../../../assets/closet-open.png";
+import closetOpen from "../../../assets/closet-opened.svg";
 
 const ClosetIcon = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -21,12 +21,17 @@ const ClosetIcon = () => {
     <div>
       <Link to="/closet" className="closet-link">
         {/* <h2>closet</h2> */}
-        <img
+        {/* <img
           className="closet-icon"
           src={isHovered ? closetOpen : closetClosed}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
-        />
+        /> */}
+        <div className="closet-container">
+          <img src={closetClosed} alt="Closet Closed" className="closet-svg closet-closed" />
+          <img src={closetOpen} alt="Closet Open" className="closet-svg closet-open" />
+        </div>
+
         {/* <div className="closet-icon"></div> */}
       </Link>
     </div>
